13748cd814e8991df62bcf7323de827a67498617
Ayenem <moub.ahmed@hotmail.com>
2023-12-23 12:05:49 -0500

    classified

--- example.py
+++ example.py
@@ -1,14 +1,15 @@
 import outlines
-from token_healing import debias_token_boundary
+from token_healing import TokenHealer
 
 model = outlines.models.awq("TheBloke/SOLAR-10.7B-Instruct-v1.0-uncensored-AWQ")
+token_healer = TokenHealer(model, model.tokenizer.tokenizer)
 # max_tokens = 50
 # TODO: find a better demonstrative prompt
 prompt = "What is the IP address of the Google DNS servers ? ! "
 # unguided = outlines.generate.text(model, max_tokens=max_tokens)(prompt)
 # print(f"\nUNGUIDED:\n{unguided}\n")
 
-healed_prompt = debias_token_boundary(prompt, model, model.tokenizer.tokenizer)
+healed_prompt = token_healer(prompt)
 print(f"\n{prompt=}\n{healed_prompt=}\n")
 # guided = outlines.generate.text(model, max_tokens=max_tokens)(healed_prompt)
 
--- token_healing.py
+++ token_healing.py
@@ -4,30 +4,34 @@
 from outlines.generate import regex as rgx_gen
 from re import escape
 
-def debias_token_boundary(prompt, model, tokenizer, max_candidates=10):
-    T = tokenizer; vocab = T.get_vocab()
-    prompt_toks = [*map(T.decode, T.encode(prompt))]
-    if removed_toks := trim_toks(prompt_toks, max_candidates, vocab):
-        prompt = prompt[: prompt.rindex(removed_toks[0])].rstrip()
-        for t in removed_toks:
-            prompt += rgx_gen(model, f"{escape(t)}.*", max_tokens=1)(prompt)
-    return prompt
-    # NOTE: pattern should be "^{t}.*" but outlines.interegular doesn't support `^`
+class TokenHealer:
+    def __init__(self, model, tokenizer, max_candidates=10):
+        self.model, self.tokenizer = model, tokenizer
+        self.vocab = tokenizer.get_vocab()
+        self.max_candidates = max_candidates
 
-def trim_toks(prompt_toks, max_candidates, vocab):
-    p_toks, removed_toks = trim_falsy_toks(prompt_toks), []
-    def many(lst): return len(lst) > max_candidates
-    while many([t for t in vocab if t.startswith(p_toks[-1])]):
-        removed_toks.insert(0, p_toks.pop())
-    return removed_toks
+    def __call__(self, prompt):
+        prompt_toks = [*map(self.tokenizer.decode, self.tokenizer.encode(prompt))]
+        if removed_toks := self.trim_toks(prompt_toks):
+            prompt = prompt[: prompt.rindex(removed_toks[0])].rstrip()
+            for t in removed_toks:
+                prompt += rgx_gen(self.model, f"{escape(t)}.*", max_tokens=1)(prompt)
+        return prompt
+        # NOTE: pattern should be "^{t}.*" but outlines.interegular doesn't support `^`
 
-def trim_falsy_toks(prompt_toks):
-    r_prompt_toks = prompt_toks[::-1]
-    last_truthy_tok = next(filter(None, r_prompt_toks))
-    p_toks = prompt_toks[: -r_prompt_toks.index(last_truthy_tok) or None]
-    return p_toks
+    def trim_toks(self, prompt_toks):
+        p_toks, removed_toks = self.trim_falsy_toks(prompt_toks), []
+        def many(lst): return len(lst) > self.max_candidates
+        while many([t for t in self.vocab if t.startswith(p_toks[-1])]):
+            removed_toks.insert(0, p_toks.pop())
+        return removed_toks
 
-# TODO: repetitive function args -> make this into a `TokenHealer` class
+    def trim_falsy_toks(self, prompt_toks):
+        r_prompt_toks = prompt_toks[::-1]
+        last_truthy_tok = next(filter(None, r_prompt_toks))
+        p_toks = prompt_toks[: -r_prompt_toks.index(last_truthy_tok) or None]
+        return p_toks
+
 # TODO: use `candidate_tokens` to cache the vocab indices to mask logits with.
 # see if the mode's generatefunction has a logits_mask function, otherwise check outlines impl
 # TODO: add type hinting
