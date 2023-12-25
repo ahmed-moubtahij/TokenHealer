e548807bae9f0bbff146a19cc42be1bb0e4f4b71
Ayenem <moub.ahmed@hotmail.com>
2023-12-25 00:45:58 -0500

    greedy sampling + type hinting

--- token_healing.py
+++ token_healing.py
@@ -1,38 +1,44 @@
 # https://towardsdatascience.com/the-art-of-prompt-design-prompt-boundaries-and-token-healing-3b2448b0be38
 # Due to greedy tokenization, the last prompt token harms generation when it prefixes better candidates in the model's vocab.
 # So we could trim the prompt tokens and let the model re-write them from its own vocab.
+from typing import Protocol
 from outlines.generate import regex as rgx_gen
+from outlines.generate.samplers import greedy
 from re import escape
 
-# SPIECE_UNDERLINE = "▁" # http://tinyurl.com/5xrpm655
+class Tokenizer(Protocol):
+    def __call__(self, s: str) -> dict[str, list[int]]: ...
+    def decode(self, id: int|list[int]) -> str: ...
 
 class TokenHealer:
-    def __init__(self, model, tokenizer):
+    def __init__(self, model, tokenizer: Tokenizer):
         self.model, self.tokenizer = model, tokenizer
         self.vocab = tokenizer.get_vocab()
 
-    def __call__(self, prompt):
-        prompt_toks = [*map(self.tokenizer.decode, self.tokenizer(prompt)['input_ids'])]
+    def __call__(self, prompt: str) -> str:
+        prompt_toks = [*map(
+            self.tokenizer.decode, self.tokenizer(prompt)['input_ids']
+        )]
         if removed_toks := self.trim_toks(prompt_toks):
-            tail_idx = prompt.rindex(removed_toks[0])
-            prompt = prompt[: tail_idx].rstrip()
+            prompt = prompt[: prompt.rindex(removed_toks[0])].rstrip()
             for t in removed_toks:
-                prompt += rgx_gen(self.model, f"{escape(t)}.*", max_tokens=1)(prompt)
+                prompt += rgx_gen(
+                    self.model, f"{escape(t)}.*", max_tokens=1, sampler=greedy
+                )(prompt)
         return prompt
         # NOTE: pattern should be "^{t}.*" but outlines.interegular doesn't support `^`
         # https://discord.com/channels/1182316225284554793/1182317824170020895/1188172732735701123
 
-    def trim_toks(self, prompt_toks):
+    def trim_toks(self, prompt_toks: list[str]) -> list[str]:
         p_toks, removed_toks = self.trim_falsy_toks(prompt_toks), []
         while len([t for t in self.vocab if t.startswith(p_toks[-1])]) > 1:
             removed_toks.insert(0, p_toks.pop())
         return removed_toks
 
-    def trim_falsy_toks(self, prompt_toks):
+    def trim_falsy_toks(self, prompt_toks: list[str]) -> list[str]:
         truthy_idx = next(i for i, t in enumerate(reversed(prompt_toks)) if t)
         return prompt_toks[: -truthy_idx or None]
 
-# TODO: add type hinting
 # TODO: use `candidate_tokens` to cache the vocab indices to mask logits with.
 # see if the mode's generatefunction has a logits_mask function, otherwise check outlines impl
 # TODO: Can the outlines logic be inlined and the dependency removed?
