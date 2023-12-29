242be8d3573ce1038ec93001aea72a7f2913f8c9
Ayenem <moub.ahmed@hotmail.com>
2023-12-28 20:04:34 -0500

    lint changes

--- token_healing.py
+++ token_healing.py
@@ -1,39 +1,26 @@
 from collections import deque
 from itertools import dropwhile
-from typing import Protocol
 
 from transformers.generation import PrefixConstrainedLogitsProcessor, MaxLengthCriteria
 from pygtrie import CharTrie
 
-class Tokenizer(Protocol):
-    def __call__(self, s: str) -> dict[str, list[int]]: ...
-    def batch_decode(self, ids: list[int]) -> list[str]: ...
-    def get_vocab(self) -> dict[str, int]: ...
-
 class TokenBoundaryHealer:
-    """
-    https://towardsdatascience.com/the-art-of-prompt-design-prompt-boundaries-and-token-healing-3b2448b0be38
-    The last prompt token harms generation when it prefixes better candidates in the model's vocab.
-    So we trim the prompt tokens and let the model re-write them from its own vocab.
-    """
-    def __init__(self, model, tokenizer: Tokenizer):
+    def __init__(self, model, tokenizer):
         self.model, self.tokenizer = model, tokenizer
         self.vocab_trie = CharTrie(tokenizer.get_vocab())
 
     def __call__(self, prompt: str) -> str:
-        prompt_tok_ids = self.tokenizer(prompt)['input_ids']
-        prompt_toks = self.tokenizer.batch_decode(prompt_tok_ids)
+        p_tok_ids = self.tokenizer(prompt)['input_ids']
+        prompt_toks = self.tokenizer.batch_decode(p_tok_ids)
         if tok_stack := self.pop_toks(prompt_toks):
-            trimmed_prompt = prompt[: prompt.rindex(tok_stack[0])].rstrip()
-            prompt_ids = self.tokenizer(
-                trimmed_prompt,
-                return_tensors='pt', # type: ignore
-            )['input_ids'].to('cuda:0') # type: ignore
+            p = prompt[: prompt.rindex(tok_stack[0])].rstrip()
+            prompt_ids = self.tokenizer(p, return_tensors='pt').input_ids.cuda()
             max_length_1 = MaxLengthCriteria(1)
             while tok_stack:
                 prefix = tok_stack.popleft()
                 logits_processor = PrefixConstrainedLogitsProcessor(
-                    lambda *_: self.vocab_trie.values(prefix=prefix), num_beams=1,
+                    lambda *_: self.vocab_trie.values(prefix=prefix), #pyright: ignore
+                    num_beams=1,
                 )
                 prompt_ids = self.model.greedy_search(
                     prompt_ids,
@@ -41,10 +28,7 @@ class TokenBoundaryHealer:
                     stopping_criteria=max_length_1,
                     pad_token_id=self.model.config.pad_token_id,
                 )
-            prompt = self.tokenizer.batch_decode(
-                prompt_ids,
-                skip_special_tokens=True, # type: ignore
-            )[0]
+            prompt = self.tokenizer.batch_decode(prompt_ids, skip_special_tokens=True)[0]
         return prompt
 
     def pop_toks(self, prompt_toks: list[str]) -> deque[str]:
