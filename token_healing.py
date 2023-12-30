5708d05c0115346238783c3f868b5f3e53d0e905
Ayenem <moub.ahmed@hotmail.com>
2023-12-30 00:23:37 -0500

    rm always true conditional

--- token_healing.py
+++ token_healing.py
@@ -12,18 +12,17 @@ class TokenBoundaryHealer:
 
     def __call__(self, prompt: str) -> str:
         trimmed_prompt, trimmed_toks_alts = self.trim_prompt(prompt)
-        if trimmed_toks_alts:
-            prompt_ids = self.encode(trimmed_prompt, return_tensors='pt').cuda()
-            max_length_1 = MaxLengthCriteria(1)
-            def logits_rule(f): return PrefixConstrainedLogitsProcessor(f, num_beams=1)
-            for tok_alts in trimmed_toks_alts:
-                prompt_ids = self.model.greedy_search(
-                    prompt_ids,
-                    logits_processor=logits_rule(lambda *_, allowed_toks=tok_alts: allowed_toks),
-                    stopping_criteria=max_length_1,
-                    pad_token_id=self.model.config.pad_token_id,
-                )
-            prompt = self.batch_decode(prompt_ids, skip_special_tokens=True)[0]
+        prompt_ids = self.encode(trimmed_prompt, return_tensors='pt').cuda()
+        max_length_1 = MaxLengthCriteria(1)
+        def logits_rule(f): return PrefixConstrainedLogitsProcessor(f, num_beams=1)
+        for tok_alts in trimmed_toks_alts:
+            prompt_ids = self.model.greedy_search(
+                prompt_ids,
+                logits_processor=logits_rule(lambda *_, allowed_toks=tok_alts: allowed_toks),
+                stopping_criteria=max_length_1,
+                pad_token_id=self.model.config.pad_token_id,
+            )
+        prompt = self.batch_decode(prompt_ids, skip_special_tokens=True)[0]
         return prompt
 
     def trim_prompt(self, prompt: str) -> tuple[str, list[list[int]]]:
