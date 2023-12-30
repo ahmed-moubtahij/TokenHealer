67305202f8b3983f3c38417a045e93aec6064aef
Ayenem <moub.ahmed@hotmail.com>
2023-12-29 20:41:59 -0500

    simpler `trim_prompt()` + qol

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # What does this do?
 
-It rectifies the [token boundary bias](https://towardsdatascience.com/the-art-of-prompt-design-prompt-boundaries-and-token-healing-3b2448b0be38) of tokenizers.
-Example: Given a prompt with a partial url ending with `:`. The model might have seen the desired `://` as a single token in training, but seeing just `:` tells it "the next token is likely not `//`, otherwise the token would've been `://`".
-It also addresses the problem of output sensitivity to prompts ending with punctuation or whitespace.
+It rectifies the [token boundary bias](https://towardsdatascience.com/the-art-of-prompt-design-prompt-boundaries-and-token-healing-3b2448b0be38) in greedy tokenization.
+Example: Given a prompt with a partial url ending with `:`. The model might have seen the desired `://` as a single token in training, but seeing just `:` tells it: "the next token is likely not `//`, otherwise the token would've been `://`".
+This includes the problem of output sensitivity to prompts ending with whitespace or punctuation.
 
 # What do I need to use this?
 
-Python 3.10^ and `pip install pygtrie`.
-
+Python 3.10^ + `pip install .` should pick up `[tool.poetry.dependencies]` from `pyproject.toml`, which are `transformers` and `pygtrie`.
 
 # How do I use this?
 `example.py`.
--- token_healing.py
+++ token_healing.py
@@ -1,6 +1,4 @@
-from collections import deque
-from itertools import dropwhile
-
+from itertools import takewhile
 from transformers.generation import PrefixConstrainedLogitsProcessor, MaxLengthCriteria
 from pygtrie import CharTrie
 
@@ -9,35 +7,35 @@ class TokenBoundaryHealer:
     def __init__(self, model, tokenizer):
         self.model, self.tokenizer = model, tokenizer
         self.vocab_trie = CharTrie(tokenizer.get_vocab())
+        self.encode, self.decode = tokenizer.encode, tokenizer.decode
+        self.batch_decode = tokenizer.batch_decode
 
     def __call__(self, prompt: str) -> str:
-        p_tok_ids = self.tokenizer(prompt)['input_ids']
-        trimmed_prompt, extension_options = self.trim_prompt(p_tok_ids)
-        if extension_options:
-            prompt_ids = self.tokenizer(
-                trimmed_prompt, return_tensors='pt'
-            ).input_ids.cuda()
+        trimmed_prompt, trimmed_toks_alts = self.trim_prompt(prompt)
+        if trimmed_toks_alts:
+            prompt_ids = self.encode(trimmed_prompt, return_tensors='pt').cuda()
             max_length_1 = MaxLengthCriteria(1)
-            for prefixes in extension_options:
-                logits_proc = PrefixConstrainedLogitsProcessor(lambda *_: prefixes, num_beams=1)
+            def logits_rule(f): return PrefixConstrainedLogitsProcessor(f, num_beams=1)
+            for tok_alts in trimmed_toks_alts:
                 prompt_ids = self.model.greedy_search(
                     prompt_ids,
-                    logits_processor=logits_proc,
+                    logits_processor=logits_rule(lambda *_, allowed_toks=tok_alts: allowed_toks),
                     stopping_criteria=max_length_1,
                     pad_token_id=self.model.config.pad_token_id,
                 )
-            prompt = self.tokenizer.batch_decode(prompt_ids, skip_special_tokens=True)[0]
+            prompt = self.batch_decode(prompt_ids, skip_special_tokens=True)[0]
         return prompt
 
-    def trim_prompt(self, tok_ids: list[int]) -> tuple[str, deque[list[int]]]:
-        prompt_toks = self.tokenizer.batch_decode(tok_ids)
-        truthy_toks = dropwhile(lambda t: not t, reversed(prompt_toks))
-        p_toks = deque(truthy_toks); p_toks.reverse()
+    def trim_prompt(self, prompt: str) -> tuple[str, list[list[int]]]:
+        prompt_tok_ids = self.encode(prompt, add_special_tokens=False)
+        prompt_toks = self.batch_decode(prompt_tok_ids)
 
-        extension_options: deque[list[int]] = deque()
-        while len(options := self.vocab_trie.values(prefix=p_toks[-1])) > 1:
-            p_toks.pop()
-            extension_options.appendleft(options)
+        tail_toks_extensions = ( # ids of e.g. ['.', ':'] -> [['.', '. '], [':', '://']]
+            self.vocab_trie.values(prefix=tail_tok) for tail_tok in reversed(prompt_toks)
+        ) # temporary reversing to use tail tokens for finding alternative tokens
+        trimmed_toks_alts = [*takewhile(lambda exts: len(exts) > 1, tail_toks_extensions)]
+        trimmed_toks_alts.reverse()
 
-        trimmed_prompt = self.tokenizer.decode(tok_ids[: -len(tail_toks_alts)], skip_special_tokens=True)
-        return trimmed_prompt, extension_options
+        trimmed_tok_ids = prompt_tok_ids[: -len(trimmed_toks_alts)]
+        trimmed_prompt = self.decode(trimmed_tok_ids, skip_special_tokens=True)
+        return trimmed_prompt, trimmed_toks_alts
