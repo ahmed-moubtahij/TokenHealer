9f899e8b9b724a3499100d170a17cc07921fac78
Ayenem <moub.ahmed@hotmail.com>
2024-01-04 10:00:47 -0500

    better autodoc

--- token_healing.py
+++ token_healing.py
@@ -1,4 +1,5 @@
 from itertools import takewhile
+
 from transformers.generation import MaxLengthCriteria
 from transformers.generation import PrefixConstrainedLogitsProcessor as AllowedToks
 from torch import IntTensor
@@ -14,21 +15,21 @@ class TokenBoundaryHealer:
     def __call__(self, prompt: str) -> str:
         prompt_ids = self.encode(prompt, return_tensors='pt').cuda()
         if alts := self.get_tail_alts(prompt_ids):
-            healed_ids = self.regen_toks(prompt_ids[:, : -len(alts) or None], alts)
+            trimmed_ids = prompt_ids[:, : -len(alts) or None]
+            healed_ids = self.regenerate_tokens(trimmed_ids, alts)
             prompt = self.decode(healed_ids.squeeze(), skip_special_tokens=True)
         return prompt
 
     def get_tail_alts(self, prompt_ids: IntTensor) -> list[list[int]]:
         prompt_toks = [*map(self.decode, prompt_ids.squeeze())]
-
-        tail_toks_extensions = ( # ids of e.g. ['.', ':'] -> [['.', '. '], [':', '://']]
-            self.vocab_trie.values(prefix=tail_tok.lstrip()) for tail_tok in reversed(prompt_toks)
-        ) # querying contiguous tail tokens for alternative tokens
+        tail_toks_extensions = (
+            self.vocab_trie.values(prefix=tail_tok.lstrip())
+            for tail_tok in reversed(prompt_toks)
+        ) # retrieving alternatives for each contiguous tail token
         tail_alts = [*takewhile(lambda exts: len(exts) > 1, tail_toks_extensions)]
-
         return tail_alts
 
-    def regen_toks(self, ids: IntTensor, toks_alts: list[list[int]]) -> IntTensor:
+    def regenerate_tokens(self, ids: IntTensor, toks_alts: list[list[int]]) -> IntTensor:
         for tok_alts in reversed(toks_alts): # regenerate last trimmed toks first
             ids = self.model.greedy_search(
                 ids,
