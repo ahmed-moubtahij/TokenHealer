05690bd441b4870ff887ca1cf608793dafa666f0
Ayenem <moub.ahmed@hotmail.com>
2024-01-03 19:28:51 -0500

    trim_prompt() -> get_tail_alts() + regen_toks() + qol

--- token_healing.py
+++ token_healing.py
@@ -1,5 +1,6 @@
 from itertools import takewhile
-from transformers.generation import PrefixConstrainedLogitsProcessor, MaxLengthCriteria
+from transformers.generation import MaxLengthCriteria
+from transformers.generation import PrefixConstrainedLogitsProcessor as AllowedToks
 from torch import IntTensor
 from pygtrie import CharTrie
 
@@ -11,26 +12,28 @@ class TokenBoundaryHealer:
         self._max_length = MaxLengthCriteria(1)
 
     def __call__(self, prompt: str) -> str:
-        left_ids, toks_alts = self.trim_prompt(prompt)
-        if not toks_alts: return prompt
-        def allowed_toks(f): return PrefixConstrainedLogitsProcessor(f, num_beams=1)
-        for tok_alts in reversed(toks_alts): # regenerate last trimmed toks first
-            left_ids = self.model.greedy_search(
-                left_ids,
-                logits_processor=allowed_toks(lambda *_, alts=tok_alts: alts),
-                stopping_criteria=self._max_length,
-                pad_token_id=self.model.config.pad_token_id,
-            )
-        healed_prompt = self.decode(left_ids.squeeze(), skip_special_tokens=True)
-        return healed_prompt
-
-    def trim_prompt(self, prompt: str) -> tuple[IntTensor, list[list[int]]]:
         prompt_ids = self.encode(prompt, return_tensors='pt').cuda()
+        if alts := self.get_tail_alts(prompt_ids):
+            healed_ids = self.regen_toks(prompt_ids[:, : -len(alts) or None], alts)
+            prompt = self.decode(healed_ids.squeeze(), skip_special_tokens=True)
+        return prompt
+
+    def get_tail_alts(self, prompt_ids: IntTensor) -> list[list[int]]:
         prompt_toks = [*map(self.decode, prompt_ids.squeeze())]
 
         tail_toks_extensions = ( # ids of e.g. ['.', ':'] -> [['.', '. '], [':', '://']]
             self.vocab_trie.values(prefix=tail_tok.lstrip()) for tail_tok in reversed(prompt_toks)
         ) # querying contiguous tail tokens for alternative tokens
-        trimmed_toks_alts = [*takewhile(lambda exts: len(exts) > 1, tail_toks_extensions)]
+        tail_alts = [*takewhile(lambda exts: len(exts) > 1, tail_toks_extensions)]
 
-        return prompt_ids[:, : -len(trimmed_toks_alts) or None], trimmed_toks_alts
+        return tail_alts
+
+    def regen_toks(self, ids: IntTensor, toks_alts: list[list[int]]) -> IntTensor:
+        for tok_alts in reversed(toks_alts): # regenerate last trimmed toks first
+            ids = self.model.greedy_search(
+                ids,
+                logits_processor=AllowedToks(lambda *_, alts=tok_alts: alts, 1),
+                stopping_criteria=self._max_length,
+                pad_token_id=self.model.config.pad_token_id,
+            )
+        return ids
