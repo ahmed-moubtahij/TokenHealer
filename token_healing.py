9d6f56a434de12ee046202eb2abf6359f55aeba1
Ayenem <moub.ahmed@hotmail.com>
2024-01-05 13:54:12 -0500

    fixed toks->ids in sequence_bias; now possible id misalignment

--- token_healing.py
+++ token_healing.py
@@ -14,14 +14,14 @@ class TokenBoundaryHealer:
         self.encode, self.decode = tokenizer.encode, tokenizer.decode
         self.batch_decode = tokenizer.batch_decode
         self.max_length_1 = MaxLengthCriteria(1)
-        self.seq_mask = { (tok,): float('-inf') for tok in self.vocab }
+        self.seq_mask = { tok_id: float('-inf') for tok_id in self.vocab.values() }
+        # NOTE: why is self.model.config.vocab_size != tokenizer.vocab_size
 
     def __call__(self, prompt: str) -> str:
         prompt_ids = self.encode(prompt, add_special_tokens=False, return_tensors='pt').cuda()
         prompt_toks = [self.decode(t).lstrip() for t in prompt_ids.squeeze()]
         if alts := self.get_tail_alts(prompt_toks):
-            trimmed_ids = prompt_ids[:, : -len(alts) or None]
-            healed_ids = self.regenerate_tokens(trimmed_ids, alts, prompt_toks)
+            healed_ids = self.regenerate_tokens(prompt_ids, alts)
             prompt = self.decode(healed_ids.squeeze(), skip_special_tokens=True)
         return prompt
 
@@ -34,18 +34,23 @@ class TokenBoundaryHealer:
         tail_alts = [*takewhile(lambda exts: len(exts) > 1, tail_toks_extensions)]
         return tail_alts
 
-    def regenerate_tokens(
-        self, ids: IntTensor, toks_alts: list[list[int]], prompt_toks: list[str]
-    ) -> IntTensor:
-        toks_to_sub = prompt_toks[-len(toks_alts): ]
+    def regenerate_tokens(self, prompt_ids: IntTensor, toks_alts: list[list[int]]) -> IntTensor:
+        ids = prompt_ids[:, : -len(toks_alts) or None] # trim prompt ids
+        toks_to_sub = prompt_ids.squeeze()[-len(toks_alts): ].tolist()
+        seq_mask = {i: float('-inf') for i in range(self.model.config.vocab_size)}
         for tok, tok_alts in zip(toks_to_sub, reversed(toks_alts)): # regenerate last trimmed toks first
-            sequence_bias = {(self.decode(alt),): 0.0 for alt in tok_alts}
+            sequence_bias = {(alt,): 0.0 for alt in tok_alts}
             # sequence_bias[(tok,)] += 1/len(tok_alts)
             # sequence_bias[(tok,)] += len(tok_alts)
             sequence_bias[(tok,)] += float('inf')
+            # sequence_bias = self.seq_mask | sequence_bias
+            # NOTE: Token ids might be misaligned with the model's logit indices
+            # NOTE: len(ids.scores[0].squeeze()) == self.model.config.vocab_size == 102400
+            #       so this should be the size of sequence_bias
+            sequence_bias = seq_mask | sequence_bias
             ids = self.model.greedy_search(
                 ids,
-                sequence_bias = self.seq_mask | sequence_bias,
+                sequence_bias = sequence_bias,
                 stopping_criteria=self.max_length_1,
                 pad_token_id=self.model.config.pad_token_id,
                 # normalize_logits=True,
