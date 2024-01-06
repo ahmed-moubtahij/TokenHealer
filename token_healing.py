3f31456a3525a7d49fe9993cb152bd7995c811c4
Ayenem <moub.ahmed@hotmail.com>
2024-01-06 11:44:37 -0500

    limiting aggressive healing with sequence_bias

--- token_healing.py
+++ token_healing.py
@@ -1,50 +1,41 @@
 from itertools import takewhile
 
-from transformers.generation import MaxLengthCriteria
+from transformers.generation import GenerationConfig
 from torch import IntTensor
 from pygtrie import CharTrie
 
 class TokenBoundaryHealer:
 
     def __init__(self, model, tokenizer):
-        self.vocab = tokenizer.get_vocab()
-        self.vocab_trie = CharTrie(self.vocab)
-        self.model = model
-        self.tokenizer = tokenizer
+        self.model, self.vocab = model, CharTrie(tokenizer.get_vocab())
         self.encode, self.decode = tokenizer.encode, tokenizer.decode
-        self.batch_decode = tokenizer.batch_decode
-        self.max_length_1 = MaxLengthCriteria(1)
-        # NOTE: why is self.model.config.vocab_size != tokenizer.vocab_size
 
     def __call__(self, prompt: str) -> str:
         prompt_ids = self.encode(prompt, add_special_tokens=False, return_tensors='pt').cuda()
-        prompt_toks = [self.decode(t).lstrip() for t in prompt_ids.squeeze()]
-        if alts := self.get_tail_alts(prompt_toks):
-            healed_ids = self.regenerate_tokens(prompt_ids, alts)
-            prompt = self.decode(healed_ids.squeeze(), skip_special_tokens=True)
+        if alt_tok_ids := self.get_tail_alts(prompt_ids):
+            healed_ids = self.regenerate_tokens(prompt_ids, alt_tok_ids)
+            prompt = self.decode(healed_ids[0], skip_special_tokens=True)
         return prompt
 
-    # def get_tail_alts(self, prompt_ids: IntTensor) -> list[list[int]]:
-    def get_tail_alts(self, prompt_toks: list[str]) -> list[list[int]]:
+    def get_tail_alts(self, prompt_ids: IntTensor) -> list[list[int]]:
+        prompt_toks = [self.decode(t).lstrip() for t in prompt_ids[0]]
         tail_toks_extensions = (
-            self.vocab_trie.values(prefix=tail_tok.lstrip())
-            for tail_tok in reversed(prompt_toks)
+            self.vocab.values(prefix=tail_tok) for tail_tok in reversed(prompt_toks)
         ) # retrieving alternatives for each contiguous tail token
         tail_alts = [*takewhile(lambda exts: len(exts) > 1, tail_toks_extensions)]
         return tail_alts
 
-        ids = prompt_ids[:, : -len(alt_tok_ids)] # trim prompt ids
-                pad_token_id=self.model.config.pad_token_id,
-                # normalize_logits=True,
-                return_dict_in_generate=True,
-                output_scores=True,
-            )
-            ids = ids.sequences
-            # # Regenerating pre-existing substrings might be useful for aligning token ids
-            # if not (new_tok := self.decode(ids[0][-1])) in prompt:
-            #     # Regenerating a non-pre-existing substring; need to preserve prompt integrity
-            #     # This is where you somehow preserve `http` over `https`
-            #     # scores = ids.scores[0].squeeze() # idk what to do with this yet
-            #     pass
-            # ids = ids.sequences
+    def regenerate_tokens(self, prompt_ids: IntTensor, alt_tok_ids: list[list[int]]) -> IntTensor:
+        if not (n_alts := len(alt_tok_ids)): raise ValueError("Expecting alternative token ids")
+        ids = prompt_ids[:, : -n_alts] # trim prompt ids
+        trimmed_toks = (e.item() for e in prompt_ids[0][-n_alts: ])
+        generation_config = GenerationConfig(
+            max_new_tokens=1, pad_token_id=self.model.config.pad_token_id,
+        ) # TODO: define bos_token_id in config for the empty `ids` case
+        for trimmed_tok, tok_alts in zip(trimmed_toks, reversed(alt_tok_ids)):
+            sequence_bias = {(tok,): 5.0 for tok in tok_alts}
+            sequence_bias[(trimmed_tok,)] += 1.0 # limit aggressive healing e.g. 'http'->'https'
+            # NOTE: consider `old_tok = tokenize(sequence_to_date)[-1]` for re-aligning ids
+            generation_config.update(sequence_bias=sequence_bias)
+            ids = self.model.generate(ids, generation_config=generation_config)
         return ids
