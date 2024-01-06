0a057bf38c3fd9ef73fdeb3bec2a0e6b167ff610
Ayenem <moub.ahmed@hotmail.com>
2024-01-06 17:36:02 -0500

    wip; empty ids case

--- token_healing.py
+++ token_healing.py
@@ -1,7 +1,7 @@
 from itertools import takewhile
 
 from transformers.generation import GenerationConfig
-from torch import IntTensor
+from torch import Tensor, int64
 from pygtrie import CharTrie
 
 class TokenBoundaryHealer:
@@ -12,12 +12,13 @@ class TokenBoundaryHealer:
 
     def __call__(self, prompt: str) -> str:
         prompt_ids = self.encode(prompt, add_special_tokens=False, return_tensors='pt').cuda()
+        # TODO: in tests[1], instead of 1189 'read', `trimmed_tok` is 1274 ' read'  beacause that's how it is in `prompt_ids`
         if alt_tok_ids := self.get_tail_alts(prompt_ids):
             healed_ids = self.regenerate_tokens(prompt_ids, alt_tok_ids)
             prompt = self.decode(healed_ids[0], skip_special_tokens=True)
         return prompt
 
-    def get_tail_alts(self, prompt_ids: IntTensor) -> list[list[int]]:
+    def get_tail_alts(self, prompt_ids: Tensor) -> list[list[int]]:
         prompt_toks = [self.decode(t).lstrip() for t in prompt_ids[0]]
         tail_toks_extensions = (
             self.vocab.values(prefix=tail_tok) for tail_tok in reversed(prompt_toks)
@@ -25,13 +26,17 @@ class TokenBoundaryHealer:
         tail_alts = [*takewhile(lambda exts: len(exts) > 1, tail_toks_extensions)]
         return tail_alts
 
-    def regenerate_tokens(self, prompt_ids: IntTensor, alt_tok_ids: list[list[int]]) -> IntTensor:
+    def regenerate_tokens(self, prompt_ids: Tensor, alt_tok_ids: list[list[int]]) -> Tensor:
         if not (n_alts := len(alt_tok_ids)): raise ValueError("Expecting alternative token ids")
-        ids = prompt_ids[:, : -n_alts] # trim prompt ids
+        if n_alts == len(prompt_ids[0]): # start from bos if all prompt tokens have alternatives
+            ids = Tensor([[self.model.config.bos_token_id]]).to(int64).cuda()
+        else:
+            ids = prompt_ids[:, : -n_alts] # trim prompt ids
         trimmed_toks = (e.item() for e in prompt_ids[0][-n_alts: ])
         generation_config = GenerationConfig(
-            max_new_tokens=1, pad_token_id=self.model.config.pad_token_id,
-        ) # TODO: define bos_token_id in config for the empty `ids` case
+            max_new_tokens=1,
+            pad_token_id=self.model.config.pad_token_id,
+        ) # TODO: fix empty `ids` case
         for trimmed_tok, tok_alts in zip(trimmed_toks, reversed(alt_tok_ids)):
             sequence_bias = {(tok,): 5.0 for tok in tok_alts}
             sequence_bias[(trimmed_tok,)] += 1.0 # limit aggressive healing e.g. 'http'->'https'
