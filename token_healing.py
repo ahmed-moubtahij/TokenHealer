736343732315c0885ac5843cef15287e46df7892
Ayenem <moub.ahmed@hotmail.com>
2024-01-13 19:39:02 -0500

    spacing for readability

--- token_healing.py
+++ token_healing.py
@@ -14,12 +14,15 @@ class TokenBoundaryHealer:
 
     def __call__(self, prompt: str) -> str:
         prompt_ids = self.encode(prompt, add_special_tokens=False)
+
         tail_tok = self.decode(prompt_ids[-1]).replace(' ', self.space_tok)
         seq_bias = {(alt_tok,): 10.0 for alt_tok in self.vocab.values(prefix=tail_tok)}
         if not seq_bias: return prompt
+
         seq_bias[(prompt_ids[-1],)] += 1.0 # limit aggressive healing e.g. 'http'->'https'
         ids = Tensor([prompt_ids[: -1]]).to(int64).cuda() if len(prompt_ids) > 1 else None
         self.gen_cfg.update(sequence_bias=seq_bias)
+
         healed_ids = self.model.generate(ids, generation_config=self.gen_cfg)
         healed_prompt = self.decode(healed_ids.squeeze(), skip_special_tokens=True)
         return healed_prompt
